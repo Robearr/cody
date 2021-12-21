b1c9d7f07fae796db2e68b01f239d8e0307e825b
Robearr <kissrobin6@gmail.com>
2021-12-21 16:23:39 +0100

    fixed token rendering problem

--- cody-client/src/views/IndexView.tsx
+++ cody-client/src/views/IndexView.tsx
@@ -1,12 +1,32 @@
+import { useContext, useEffect, useState } from 'react';
 import { useKeycloakLogin } from '../hooks/useKeycloakLogin';
+import { KeycloakContext } from '../providers/KeycloakProvider';
 
 interface IndexViewProps {};
 
 export const IndexView: React.FC<IndexViewProps> = () => {
 
   useKeycloakLogin();
+  const { keycloak } = useContext(KeycloakContext);
+  const [token, setToken] = useState<string>('');
+
+  useEffect(() => {
+    keycloak.onReady = () => {
+      if (keycloak?.token) {
+        setToken(keycloak.token);
+      }
+    };
+  }, [keycloak]);
+
+  const copyToken = (): void => {
+    navigator.clipboard.writeText(token);
+    alert('Token vágólapra másolva!');
+  };
 
   return (
-    <h1>Hello there</h1>
+    <div>
+      <h1>Token</h1>
+      <code onClick={copyToken} style={{ cursor: 'pointer' }} >{token}</code>
+    </div>
   );
 };
