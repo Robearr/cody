baea25e9c94bf945dfe527b89d3629bfb09f36bf
Robearr <kissrobin6@gmail.com>
2022-01-18 10:29:13 +0100

    added temp menu

--- cody-client/src/App.tsx
+++ cody-client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Alert, Snackbar, AlertColor } from '@mui/material';
+import { Alert, Snackbar, AlertColor, AppBar, Link, Typography } from '@mui/material';
 import { useContext } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Message, MessageContext } from './providers/MessageProvider';
@@ -8,6 +8,7 @@ import { NewTaskView } from './views/NewTaskView';
 import { TasksView } from './views/TasksView';
 import { CountDownCloseButton } from './ui/CountDownCloseButton';
 
+import logo from './images/logo.png';
 import './styles/common.scss';
 
 function App() {
@@ -16,6 +17,13 @@ function App() {
 
   return (
     <div>
+      <AppBar position='static' style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}>
+        <Link href='/'>
+          <img src={logo} alt='logo' style={{ width: '20%' }}/>
+        </Link>
+        <Link href='/tasks' color='inherit'><Typography variant='h6'>Feladatok</Typography></Link>
+        <Link href='/new-task' color='inherit'><Typography variant='h6'>Új feladat</Typography></Link>
+      </AppBar>
       <Router /*basename={process.env.PUBLIC_URL}*/>
         <Routes>
           <Route path='/' element={<IndexView />} />
